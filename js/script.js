4eef3690b87101d3f1b1efd679fad504f1798ec0
MUsmanAliWafa <musmanaliwafa@gmail.com>
2023-08-17 13:07:10 +0700

    menambahkan model box sementara

--- js/script.js
+++ js/script.js
@@ -78,6 +78,40 @@ form.addEventListener('submit', (e) => {
 });
 
 
+// modal start
+// Get the modal
+const modal = document.querySelector("#myModal1");
+
+// Get the button that opens the modal
+const btn = document.querySelector("#myBtn1");
+
+// Get the <span> element that closes the modal
+const close = document.querySelector(".close");
+
+const tutupBtn = document.querySelector(".tutup");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+
+close.onclick = function () {
+  modal.style.display = "none";
+}
+tutupBtn.onclick = function () {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// modal end 
+
+
 
 // tahun
 let currentYear = new Date().getFullYear();
