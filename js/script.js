b704727c9e4c762afc9d6853a5288559f281a08b
MUsmanAliWafa <musmanaliwafa@gmail.com>
2023-08-19 01:22:53 +0700

    1

--- js/script.js
+++ js/script.js
@@ -82,25 +82,26 @@ form.addEventListener('submit', (e) => {
 let preveiwContainer = document.querySelector('.products-preview');
 let previewBox = preveiwContainer.querySelectorAll('.preview');
 
-document.querySelectorAll('.products-container .product').forEach(product =>{
-  product.onclick = () =>{
+document.querySelectorAll('.products-container .product').forEach(product => {
+  product.onclick = () => {
     preveiwContainer.style.display = 'flex';
     let name = product.getAttribute('data-name');
-    previewBox.forEach(preview =>{
+    previewBox.forEach(preview => {
       let target = preview.getAttribute('data-target');
-      if(name == target){
+      if (name == target) {
         preview.classList.add('active');
       }
     });
   };
 });
 
-previewBox.forEach(close =>{
-  close.querySelector('.fa-times').onclick = () =>{
+previewBox.forEach(close => {
+  close.querySelector('.fa-times').onclick = () => {
     close.classList.remove('active');
     preveiwContainer.style.display = 'none';
   };
 });
+
 // modal end 
 
 
